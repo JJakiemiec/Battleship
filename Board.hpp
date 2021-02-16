31624e7393a2391bbd0d271abd6d90ad23fcd56a
Jacob Jakiemiec <jjakiemiec@gmail.com>
2021-02-16 01:09:08 -0900

    Add getPrintableChar and getPrintableBoard started

--- Board.hpp
+++ Board.hpp
@@ -19,6 +19,10 @@ using std::vector;
 //      3 : Cruiser     (size 3)
 //      4 : Battleship  (size 4)
 //      5 : Carrier     (size 5)
+//
+// Boardtype holds the what is the type of the current board
+//		0 : player's placable board, where the player places ships and records opponent's shots
+//		1 : player's guessing board, where the player keeps tracks of hits and misses of opponent's ships
 
 using BoardType = vector<vector<std::pair<int,int>>>; 
 // *********************************************************************
@@ -28,17 +32,20 @@ class Board {
 
 // ***** Ship: Constructors, Destructors ******
 public:
-   Board() : _board(10, vector<std::pair<int,int>>(10, std::make_pair(0,0))) {}
- 
+	Board() : _board(10, vector<std::pair<int,int>>(10, std::make_pair(0,0))), _boardType(0) {}
+	Board(int boardType) : _board(10, vector<std::pair<int, int>>(10, std::make_pair(0, 0))), _boardType(boardType) {}
 // ***** Ship: Public Member Functions *****
 public:
     BoardType getBoard();
+	vector<vector<char>> getPrintableBoard();
     void shootBoard(int x, int y);
     void placeShip(Ship ship);
+	char getPrintableChar(int i, int j);
 
 // ***** Ship: Private Data Members *****
 private:
     BoardType _board;
+	int _boardType;						
 };
 
 #endif
